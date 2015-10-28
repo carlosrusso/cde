4c6b59abbb7707465fac6cd6960ee7580d71fc2d
Carlos Russo <carlos.russo@webdetails.pt>
2015-10-28 14:54:54 +0000

    [TEST] Getting the test framework to run

---
+++ cde-core/test-js/components/Map/MapEngine-spec.js
@@ -0,0 +1,10 @@
+define([
+  'cde/components/Map/engines/openlayers2/mapengine-openlayers',
+  'cdf/lib/jquery'
+], function(MapEngineOL2, $) {
+
+  describe('NewMapComponent/MapEngine/OpenLayers2', function() {
+
+  });
+
+});
