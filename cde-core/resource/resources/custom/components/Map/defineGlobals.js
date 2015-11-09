8597ad2ccd882f068093ff176c58b010774d3a8f
Carlos Russo <carlos.russo@webdetails.pt>
2015-11-09 21:25:11 +0000

    Created an aggregated version of the modular Map component, as well as some shims to mock the modular system

---
+++ cde-core/resource/resources/custom/components/Map/defineGlobals.js
@@ -0,0 +1,32 @@
+define('cdf/lib/jquery', function () {
+  return $;
+});
+define('amd!cdf/lib/underscore', function () {
+  return _;
+});
+define('amd!cdf/lib/backbone', function () {
+  return Backbone;
+});
+define('cdf/AddIn', function () {
+  return AddIn;
+});
+
+define('cdf/components/BaseComponent', function () {
+  return BaseComponent;
+});
+define('cdf/UnmanagedComponent', function () {
+  return UnmanagedComponent;
+});
+define('cdf/Dashboard.Clean', function () {
+  Dashboards.registerGlobalAddIn = Dashboards.registerGlobalAddIn || Dashboards.registerAddIn;
+  return Dashboards;
+});
+
+define('cdf/Logger', function () {
+  return {
+    log: Dashboards.log,
+    debug: Dashboards.debug,
+    error: Dashboards.error,
+    warn: Dashboards.warn
+  };
+});
