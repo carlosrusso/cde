f7c2636425f9486712b5fd724fb9096ac5f6c5d0
Carlos Russo <carlos.russo@webdetails.pt>
2015-11-16 15:58:46 +0000

    Produced legacy version of the Map component

--- cde-core/resource/resources/custom/components/Map/defineGlobals.js
+++ cde-core/resource/resources/custom/components/Map/defineGlobals.js
@@ -1,46 +1,44 @@
-var myglobals = {
-  'cdf/lib/jquery': $,
-  'amd!cdf/lib/underscore': _,
-  'amd!cdf/lib/backbone': Backbone,
-  'cdf/AddIn': AddIn,
-  'cdf/lib/Base': Base,
-  'cdf/lib/BaseSelectionTree': TreeFilter.Models.SelectionTree,
-  'cdf/components/UnmanagedComponent': UnmanagedComponent,
-  'cdf/lib/OpenLayers':OpenLayers,
-  'cdf/lib/mustache': Mustache,
-  'css!./Map': ''
-};
+(function () {
 
-
-for (var p in myglobals){
-  define(p, function(){
-    return myglobals[p];
-  });
-}
-var CONTEXT_PATH = '';
-
-define('cdf/Dashboard.Clean', function () {
-  Dashboards.registerGlobalAddIn = Dashboards.registerGlobalAddIn || Dashboards.registerAddIn;
-  return Dashboards;
-});
-
-define('cdf/Logger', function () {
-  return {
-    log: Dashboards.log,
-    debug: Dashboards.debug,
-    error: Dashboards.error,
-    warn: Dashboards.warn
+  var myglobals = {
+    'cdf/lib/jquery': $,
+    'amd!cdf/lib/underscore': _,
+    'amd!cdf/lib/backbone': Backbone,
+    'cdf/AddIn': AddIn,
+    'cdf/lib/mustache': Mustache,
+    'cdf/lib/Base': Base,
+    'cdf/lib/BaseEvents': BaseEvents,
+    'cdf/components/UnmanagedComponent': UnmanagedComponent,
+    'cdf/lib/BaseSelectionTree': TreeFilter.Models.SelectionTree,
+    'cdf/lib/OpenLayers': OpenLayers,
+    'css!./Map': ''
   };
-});
 
-define('text!./ControlPanel.html', function () {
-  return '<div class="map-control-panel">'+
-    '<div class="map-control-button map-control-zoom-in"></div>'+
-    '<div class="map-control-button map-control-zoom-out"></div>'+
-    '<div class="map-controls-mode {{mode}}">'+
-    '<div class="map-control-button map-control-pan"></div>'+
-    '<div class="map-control-button map-control-zoombox"></div>'+
-    '<div class="map-control-button map-control-select"></div>'+
-    '</div>'+
-    '</div>';
-});
+
+  CONTEXT_PATH = Dashboards.getWebAppPath();
+  for (var p in myglobals) {
+    define(p, function () {
+      return myglobals[p];
+    });
+  }
+
+  define('cdf/Dashboard.Clean', function () {
+    Dashboards.registerGlobalAddIn = Dashboards.registerGlobalAddIn || Dashboards.registerAddIn;
+    return Dashboards;
+  });
+
+  define('cdf/Logger', function () {
+    return {
+      log: Dashboards.log,
+      debug: Dashboards.log,
+      error: Dashboards.log,
+      warn: Dashboards.log
+    };
+  });
+
+  define("text!./ControlPanel.html", [], function() {
+    return '<div class="map-control-panel">\n    <div class="map-control-button map-control-zoom-in"></div>\n    <div class="map-control-button map-control-zoom-out"></div>\n    <div class="map-controls-mode {{mode}}">\n        <div class="map-control-button map-control-pan"></div>\n        <div class="map-control-button map-control-zoombox"></div>\n        <div class="map-control-button map-control-select"></div>\n    </div>\n</div>';
+  })
+
+}());
+
